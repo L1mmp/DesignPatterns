c581bba671b2abe378773c9e665c7ccbf8bc673a
L1mmp <dima.wwe2@yandex.ru>
2022-11-08 13:34:46 +0300

    Removed unnecessary usings in DI

--- Patterns/DI/PostgreSQLDB.cs
+++ Patterns/DI/PostgreSQLDB.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.Patterns.DI
+﻿namespace DesignPatterns.Patterns.DI
 {
 	public class PostgreSQLDB : IDataSource
 	{
